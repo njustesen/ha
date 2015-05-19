f64db833e0927c61b911aa236325e08c6ba8f2bc
Niels Justesen <noju@Nielss-MacBook-Pro.local>
2015-05-19 15:01:09 +0200

    experiment

--- src/testcase/TestSuiteFinal.java
+++ src/testcase/TestSuiteFinal.java
@@ -61,7 +61,8 @@ public class TestSuiteFinal {
 		if (args[0].equals("mcts-c"))
 			MctsCTests(Integer.parseInt(args[1]), args[2]);
 		
-		
+		if (args[0].equals("mcts-c0"))
+			MctsC0(Integer.parseInt(args[1]), args[2]);
 		
 		if (args[0].equals("mcts-random"))
 			MctsRandom(Integer.parseInt(args[1]), args[2]);
